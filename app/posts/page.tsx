8376a3718c7b3706b53258070f31818cb8e726a2
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-22 16:16:55 +0900

    test

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,16 +2,29 @@ import Link from 'next/link';
 import { Post } from '@prisma/client';
 import { BASE_URL } from '@/constants';
 
-const PostListPage = async () => {
-  const postList: Post[] = await fetch(`${BASE_URL}/posts`, {
+const fetchPosts = async () => {
+  // const posts: Post[] = await fetch(`${BASE_URL}/posts`, {
+  //   cache: 'no-store',
+  // }).then((res) => res.json());
+
+  const datas: {
+    id: number;
+    title: string;
+  }[] = await fetch('https://jsonplaceholder.typicode.com/todos', {
     cache: 'no-store',
   }).then((res) => res.json());
+
+  return datas;
+};
+
+const PostListPage = async () => {
+  const posts = await fetchPosts();
 
   return (
     <div>
       <h1>Post List Page</h1>
       <ul className="flex flex-col gap-2 p-2">
-        {postList.map((post) => (
+        {posts?.map((post) => (
           <li key={post.id}>
             <Link href={`/posts/${post.id}`}>
               <div className="text-blue-500 hover:underline">{post.title}</div>
